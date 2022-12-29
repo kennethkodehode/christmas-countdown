f5a10dad2e2b76ea7543c013fd2fec7999910e73
kennethkodehode <kenneth.kodehode@gmail.com>
2022-12-29 08:28:53 +0100

    Added HeroSection component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import SiteHeader          from './components/SiteHeader'
-import AboutSection        from './components/AboutSection'
-import HostsSection        from './components/HostsSection'
+import SiteHeader   from './components/SiteHeader'
+import HeroSection from './components/HeroSection'
+import AboutSection from './components/AboutSection'
+import HostsSection from './components/HostsSection'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <SiteHeader />
+      <HeroSection />
       <AboutSection />
       <HostsSection />
     </div>
---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Countdown from '../Countdown'
+
+import './styles.scss'
+
+const HeroSection: React.FC = () => (
+  <section className="hero">
+    <div className="hero__content contain-width">
+      <p className="font-festive hero__content__text">Holiday event</p>
+    </div>
+    <Countdown className="hero__countdown contain-width" />
+  </section>
+)
+
+export default HeroSection
